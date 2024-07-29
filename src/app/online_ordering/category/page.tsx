da5505de1e4b30864305d03df84e062ed108bca0
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-07-29 17:53:21 +0530

    done

--- src/app/online_ordering/category/page.tsx
+++ src/app/online_ordering/category/page.tsx
@@ -7,8 +7,8 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/app/config/firebase";
 import CategoryModels from "@/app/modal/CategoryModels";
 
-const category = () => {
-  const [category, setCategory] = useState<CategoryModels[]>([]);
+const Category: React.FC = () => {
+  const [categories, setCategories] = useState<CategoryModels[]>([]);
 
   const fetchCategory = async () => {
     try {
@@ -20,7 +20,7 @@ const category = () => {
           id: doc.id,
         };
       });
-      setCategory(categoryList);
+      setCategories(categoryList);
     } catch (error) {
       console.log(error);
     }
@@ -29,6 +29,7 @@ const category = () => {
   useEffect(() => {
     fetchCategory();
   }, []);
+
   return (
     <section className="main-bg">
       <div className="page_width h-full">
@@ -36,27 +37,24 @@ const category = () => {
           <div className="flex justify-center h-full p-10">
             <Image width={200} height={100} src={onloadImg} alt="onload img" />
           </div>
-          <div className="">
+          <div>
             <button className="text-[#fff] bg-[#ded4c4] p-3 rounded-xl font-bold">Back</button>
           </div>
-          {category?.map((item, index) => {
-            return (
-              <Link href={`/online_ordering/${item?.Name}`}>
-                <div key={index} className="flex flex-col gap-5 py-5">
-                  <div className="w-full h-[185px] relative">
-                    {/* <div className="bg-[#9efeb98a] absolute top-0 w-full h-full left-0"></div> */}
-                    <div className="bg-[#00000083] absolute top-0 w-full h-full left-0"></div>
-                    <img className="object-cover w-full h-full" src={item?.ImageUrl} alt="category image" />
-                    <p className="absolute top-[50%] left-[50%] transform text-xl font-semibold text-white">{item?.Name}</p>
-                  </div>
+          {categories?.map((item) => (
+            <Link href={`/online_ordering/${item?.Name}`} key={item?.id}>
+              <div className="flex flex-col gap-5 py-5">
+                <div className="w-full h-[185px] relative">
+                  <div className="bg-[#00000083] absolute top-0 w-full h-full left-0"></div>
+                  <img className="object-cover w-full h-full" src={item?.ImageUrl} alt="category image" />
+                  <p className="absolute top-[50%] left-[50%] transform text-xl font-semibold text-white">{item?.Name}</p>
                 </div>
-              </Link>
-            );
-          })}
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </section>
   );
 };
 
-export default category;
+export default Category;
